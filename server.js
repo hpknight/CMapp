0b4d3c5323aa4ce7398b10100d3caf66818a153e
Himan Patel <hpknight15@gmail.com>
2016-09-24 14:10:39 -0400

    users crud
    create users route for crud operations

--- server.js
+++ server.js
@@ -34,6 +34,82 @@ apiRouter.use(function(req, res, next) {
 	next();
 });
 
+apiRouter.route('/users')
+	.post(function(req, res) {
+		var user = new User();
+
+		user.name = req.body.name;
+		user.username = req.body.username;
+		user.password = req.body.password;
+
+		user.save(function(err) {
+			if (err) {
+				if (err.code === 11000) {
+					return res.json({
+						success: false,
+						message: 'A user with that username already exists.'
+					});
+				} else {
+					res.send(err);
+				}
+			}
+
+			res.json({ message: 'User created!' });
+		});
+	})
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(users);
+		});
+	});
+
+apiRouter.route('/users/:user_id')
+	.get(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(user);
+		});
+	})
+	.put(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+			if (err) {
+				res.send(err);
+			}
+
+			if (req.body.name) {
+				user.name = req.body.name;
+			}
+			if (req.body.username) {
+				user.username = req.body.username;
+			}
+			if (req.body.password) {
+				user.password = req.body.password;
+			}
+
+			user.save(function(err) {
+				if (err) {
+					res.send(err);
+				}
+				res.json({ message: 'User updated!' });
+			});
+		});
+	})
+	.delete(function(req, res) {
+		User.remove({
+			_id: req.params.user_id
+		}, function(err, user) {
+			if (err) {
+				return res.send(err);
+			}
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
 apiRouter.get('/', function(req, res) {
 	res.json({message: 'hooray! welcome to our api!'});
 });
