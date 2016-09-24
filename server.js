9394f4a23d7568e0301ac03fc2c2263b6fe8f06e
Himan Patel <hpknight15@gmail.com>
2016-09-24 14:06:34 -0400

    user schema and set up basic api
    Include additional node packages; set up basic api to start; set up
    user schema

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var User = require('./app/models/user');
+
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hello World.');
+mongoose.connect('localhost:27017/sample');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+	next();
 });
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+	res.send('Welcome to the home page!');
+});
+
+//
+// BEGIN api routes
+//
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next) {
+	console.log('Somebody just came to our app');
+	next();
+});
+
+apiRouter.get('/', function(req, res) {
+	res.json({message: 'hooray! welcome to our api!'});
+});
+
+app.use('/api', apiRouter);
+//
+// END api routes
+//
 
 app.listen(3000, function() {
 	console.log('Listening on port 3000.');
