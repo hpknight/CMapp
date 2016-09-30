a58268377ea35a2a47799aa66a7ce725b14d9aee
Himan Patel <hpknight15@gmail.com>
2016-09-30 09:39:02 -0400

    minimize and organize server.js
    Working baseline for app

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var config = require('./config');
-// var User = require('./app/models/user');
-// var jwt = require('jsonwebtoken');
-// var superSecret = 'superSecret';
 var path = require('path');
 
-var app = express();
-
-mongoose.connect(config.database);
-
+// app configuration
+// use body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'));
-
+// handle CORS requests
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -24,24 +19,28 @@ app.use(function(req, res, next) {
 	next();
 });
 
+// log all requests to the console
 app.use(morgan('dev'));
 
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+// connect to our db
+mongoose.connect(config.database);
 
-app.get('/', function(req, res) {
-	res.send('Welcome to the home page!');
-});
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
 
-//
 // BEGIN api routes
-//
 var apiRoutes = require('./app/routes/api.js');
 app.use('/api', apiRoutes);
-//
 // END api routes
-//
+
+// MAIN CATCHALL ROUTE
+// send users to frontends
+// has to be registered after api routes
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
 
 app.listen(config.port, function() {
 	console.log('Listening on port ' + config.port);
