dfe759b3a36cc53b78512bd5895648e0b0a97fa0
Himan Patel <hpknight15@gmail.com>
2016-09-30 10:48:02 -0400

    create a user service
    client side user service to communicate with backend user api

---
+++ public/app/services/user.service.js
@@ -0,0 +1,31 @@
+angular.module('userService', [])
+	.factory('User', function($http) {
+		var user = {};
+
+		// get a single user
+		user.get = function(id) {
+			return $http.get('/api/users/' + id);
+		};
+
+		// get all users
+		user.all = function() {
+			return $http.get('/api/users/');
+		};
+
+		// create a user
+		user.create = function(userData) {
+			return $http.post('/api/users/' + userData);
+		};
+
+		// update a user
+		user.update = function(id, userData) {
+			return $http.put('/api/users/' + id, userData);
+		};
+
+		// delete a user
+		user.delete = function(id) {
+			return $http.delete('/api/users/' + id);
+		};
+
+		return user;
+	});
